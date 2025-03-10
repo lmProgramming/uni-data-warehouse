8f76061dc93d3f993028b5af0a2241bb7f7698c3
lmProgramming <mikikubs@gmail.com>
2025-03-10 23:45:56 +0100

    feat: finished queries 3 and 4

--- lab02/queries.sql
+++ lab02/queries.sql
@@ -45,3 +45,41 @@ PIVOT (
 	FOR OrderYear IN ([2011], [2012], [2013], [2014])
 ) AS PivotedTable
 ORDER BY FirstName
+
+-- 3
+
+-- Zdefiniować zapytanie wyznaczające sumę kwot sprzedaży towarów oraz liczbę różnych produktów w zamówieniach w poszczególnych latach, miesiącach, dniach.
+
+SELECT YEAR(OrderDate) AS "Rok", MONTH(OrderDate) AS "Miesiąc", DAY(OrderDate) AS "Dzień", SUM(LineTotal) AS "Suma", COUNT(DISTINCT ProductID) AS "Liczba różnych produktów"
+FROM Sales.SalesOrderHeader
+JOIN Sales.SalesOrderDetail ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
+GROUP BY YEAR(OrderDate), MONTH(OrderDate), DAY(OrderDate)
+ORDER BY YEAR(OrderDate), MONTH(OrderDate), DAY(OrderDate)
+
+-- 4
+
+-- Wykorzystując polecenie CASE przygotować podsumowania do zestawienia z poprzedniego zadania tak, aby sumowane były kwoty zamówień oraz obliczana liczba
+-- różnych produktów dla poszczególnych miesięcy i dni tygodnia.
+-- Uwaga: Pamiętaj o wybraniu właściwego atrybutu funkcji datepart tak, aby zgadzała się nazwa dnia tygodnia
+
+SET DATEFIRST 1;
+SET LANGUAGE Polish;
+
+SELECT 
+	YEAR(OrderDate) AS "Rok", 
+	DATENAME(month, OrderDate) AS "Miesiąc",     
+	CASE DATEPART(dw, OrderDate)
+        WHEN 1 THEN 'Poniedziałek'
+        WHEN 2 THEN 'Wtorek'
+        WHEN 3 THEN 'Środa'
+        WHEN 4 THEN 'Czwartek'
+        WHEN 5 THEN 'Piątek'
+        WHEN 6 THEN 'Sobota'
+        WHEN 7 THEN 'Niedziela'
+    END AS "Dzień tygodnia", 
+	SUM(LineTotal) AS "Suma", 
+	COUNT(DISTINCT ProductID) AS "Liczba różnych produktów"
+FROM Sales.SalesOrderHeader
+JOIN Sales.SalesOrderDetail ON SalesOrderDetail.SalesOrderID = SalesOrderHeader.SalesOrderID
+GROUP BY YEAR(OrderDate), DATENAME(month, OrderDate), MONTH(OrderDate), DATEPART(dw, OrderDate)
+ORDER BY YEAR(OrderDate), MONTH(OrderDate), DATEPART(dw, OrderDate)
