90c184a2b169722279551e415c8da35aff258134
lmProgramming <mikikubs@gmail.com>
2025-03-23 23:03:47 +0100

    feat: 6 done

--- lec03/sql/6.sql
+++ lec03/sql/6.sql
@@ -1,15 +1,15 @@
--- Przygotuj zestawienie, w którym możliwa będzie analiza regionalna z uwzględnieniem lokalnej
--- waluty (kwoty sprzedaży w zależności od waluty i regionu).
-
-SELECT SUM(TotalDue) AS "Total order cost (USD)", ISNULL(c.ToCurrencyCode, 'USD') AS "Currency", t.Name, AVG(TotalDue) AS "Average order cost (USD)"
-FROM Sales.SalesOrderHeader s
-LEFT JOIN (
-	SELECT ToCurrencyCode, CurrencyRateID
-	FROM Sales.CurrencyRate
-) c on s.CurrencyRateID = c.CurrencyRateID
-LEFT JOIN (
-	SELECT TerritoryID, Name, CountryRegionCode, "Group"
-	FROM Sales.SalesTerritory
-) t on s.TerritoryID = t.TerritoryID
-GROUP BY t.Name, c.ToCurrencyCode
-ORDER BY 1 DESC;
+SELECT 
+    Territory.Name AS Region,
+    ISNULL(CAST(Currency.CurrencyCode AS VARCHAR(20)), 'Brak waluty') AS Waluta,
+    CountryRegionCode AS "Kod regionu",
+    SUM(SalesOrderHeader.TotalDue) AS "Wartość sprzedaży",
+    COUNT(SalesOrderHeader.SalesOrderID) AS "Liczba zamówień"
+FROM Sales.SalesTerritory AS Territory
+LEFT JOIN Sales.SalesOrderHeader 
+    ON Territory.TerritoryID = SalesOrderHeader.TerritoryID
+LEFT JOIN Sales.CurrencyRate AS CurrencyRate
+    ON SalesOrderHeader.CurrencyRateID = CurrencyRate.CurrencyRateID
+LEFT JOIN Sales.Currency AS Currency
+    ON CurrencyRate.ToCurrencyCode = Currency.CurrencyCode
+GROUP BY Territory.Name, Currency.CurrencyCode, CountryRegionCode
+ORDER BY CountryRegionCode, Territory.Name, SUM(SalesOrderHeader.TotalDue) DESC
