fbf80dd1f6c1f58cd0d427c3842db9ecb5c05a04
jantar <mikikubs@gmail.com>
2025-03-25 20:47:04 +0100

    feat: data analysis done in Python

--- lab03/data_analysis.py
+++ lab03/data_analysis.py
@@ -1,11 +1,43 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
+from os import path
 
-# Wczytanie pliku CSV
-df = pd.read_csv("dane_lista3.csv")
+dir_path: str = path.dirname(path.realpath(__file__))
 
-# Tworzenie raportu
+print(dir_path)
+df: pd.DataFrame = pd.read_csv(path.join(dir_path, "dane_lista3.csv"))
+
 profile = ProfileReport(df, explorative=True)
 
-# Zapis raportu do pliku HTML
-profile.to_file("profilowanie_danych.html")
+profile.to_file(path.join(dir_path, "python_results.html"))
+
+print(df.columns)
+for i in df.columns:
+    print(df[i].dtype)
+print(df.dtypes)
+
+df['Transaction ID'] = df['Transaction ID'].astype('str')
+
+df['Item'] = df['Item'].astype('str')
+
+df['Quantity'] = pd.to_numeric(df['Quantity'], errors='coerce')
+
+df['Price Per Unit'] = pd.to_numeric(df['Price Per Unit'], errors='coerce')
+
+df['Total Spent'] = pd.to_numeric(df['Total Spent'], errors='coerce')
+
+df['Payment Method'] = df['Payment Method'].astype('str')
+
+df['Location'] = df['Location'].astype('str')
+
+df['Transaction Date'] = pd.to_datetime(
+    df['Transaction Date'], errors='coerce')
+
+print("Updated dtypes:")
+print(df.dtypes)
+
+print(df)
+
+profile = ProfileReport(df, explorative=True)
+
+profile.to_file(path.join(dir_path, "python_results_good_data_types.html"))
