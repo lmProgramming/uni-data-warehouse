70cc2bb79eb661733f1b29ae58c953a003965f02
lmProgramming <mikikubs@gmail.com>
2025-04-10 20:18:13 +0200

    feat: 3 done

---
+++ lab04/sql/3.sql
@@ -0,0 +1,102 @@
+DELETE FROM Kubs.FACT_SALES;
+DELETE FROM Kubs.DIM_SALESPERSON;
+DELETE FROM Kubs.DIM_PRODUCT;
+DELETE FROM Kubs.DIM_CUSTOMER;
+
+INSERT INTO Kubs.DIM_CUSTOMER (
+    CustomerID,
+    FirstName,
+    LastName,
+    Title,
+    City,
+    TerritoryName,
+    CountryRegionCode,
+    [Group]
+)
+SELECT DISTINCT
+    c.CustomerID,
+    p.FirstName,
+    p.LastName,
+    p.Title,
+    a.City,
+    st.Name AS TerritoryName,
+    st.CountryRegionCode,
+    st.[Group]
+FROM Sales.Customer AS c
+LEFT JOIN Person.Person AS p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.SalesTerritory AS st ON c.TerritoryID = st.TerritoryID
+LEFT JOIN Person.BusinessEntityAddress bea ON p.BusinessEntityID = bea.BusinessEntityID 
+LEFT JOIN Person.Address AS a ON bea.AddressID = a.AddressID
+WHERE c.PersonID IS NOT NULL; 
+
+INSERT INTO Kubs.DIM_PRODUCT (
+    ProductID,
+    Name,
+    ListPrice,
+    Color,
+    SubCategoryName,
+    CategoryName,
+    Weight,
+    Size,
+    IsPurchased
+)
+SELECT DISTINCT
+    p.ProductID,
+    p.Name,
+    p.ListPrice,
+    p.Color,
+    psc.Name AS SubCategoryName,
+    pc.Name AS CategoryName,
+    p.Weight,
+    p.Size,
+    1 AS IsPurchased
+FROM Production.Product AS p
+INNER JOIN Sales.SalesOrderDetail AS sod ON p.ProductID = sod.ProductID
+LEFT JOIN Production.ProductSubcategory AS psc ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+LEFT JOIN Production.ProductCategory AS pc ON psc.ProductCategoryID = pc.ProductCategoryID;
+
+INSERT INTO Kubs.DIM_SALESPERSON (
+    SalesPersonID,
+    FirstName,
+    LastName,
+    Title,
+    Gender,
+    CountryRegionCode,
+    [Group]
+)
+SELECT
+    sp.BusinessEntityID AS SalesPersonID,
+    p.FirstName,
+    p.LastName,
+    p.Title,
+    e.Gender,
+    st.CountryRegionCode,
+    st.[Group]
+FROM Sales.SalesPerson AS sp
+INNER JOIN Person.Person AS p ON sp.BusinessEntityID = p.BusinessEntityID
+INNER JOIN HumanResources.Employee AS e ON sp.BusinessEntityID = e.BusinessEntityID
+LEFT JOIN Sales.SalesTerritory AS st ON sp.TerritoryID = st.TerritoryID;
+
+INSERT INTO Kubs.FACT_SALES (
+    ProductID,
+    CustomerID,
+    SalesPersonID,
+    OrderDate,
+    ShipDate,
+    OrderQty,
+    UnitPrice,
+    UnitPriceDiscount,
+    LineTotal
+)
+SELECT
+    sod.ProductID,
+    soh.CustomerID,
+    soh.SalesPersonID,
+    DATEPART(YEAR, soh.OrderDate) * 10000 + DATEPART(MONTH, soh.OrderDate) * 100 + DATEPART(DAY, soh.OrderDate) AS OrderDate,
+    DATEPART(YEAR, soh.ShipDate) * 10000 + DATEPART(MONTH, soh.ShipDate) * 100 + DATEPART(DAY, soh.ShipDate) AS ShipDate,
+    sod.OrderQty,
+    sod.UnitPrice,
+    sod.UnitPriceDiscount,
+    sod.LineTotal
+FROM Sales.SalesOrderDetail AS sod
+INNER JOIN Sales.SalesOrderHeader AS soh ON sod.SalesOrderID = soh.SalesOrderID;
---
+++ lab04/sql/tests.sql
@@ -0,0 +1,40 @@
+SELECT * FROM Kubs.DIM_CUSTOMER;
+SELECT * FROM Kubs.DIM_PRODUCT;
+SELECT * FROM Production.Product
+LEFT JOIN Production.ProductSubcategory ON ProductSubcategory.ProductSubcategoryID = Product.ProductSubcategoryID
+LEFT JOIN Production.ProductCategory ON ProductCategory.ProductCategoryID = ProductSubcategory.ProductCategoryID;
+
+SELECT DISTINCT
+    p.ProductID,
+    p.Name,
+    p.ListPrice,
+    p.Color,
+    psc.Name AS SubCategoryName,
+    pc.Name AS CategoryName,
+    p.Weight,
+    p.Size,
+    1 AS IsPurchased
+FROM Production.Product AS p
+INNER JOIN Sales.SalesOrderDetail AS sod ON p.ProductID = sod.ProductID
+LEFT JOIN Production.ProductSubcategory AS psc ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+LEFT JOIN Production.ProductCategory AS pc ON psc.ProductCategoryID = pc.ProductCategoryID;
+
+SELECT DISTINCT
+    p.ProductID,
+    p.Name,
+    p.ListPrice,
+    p.Color,
+    psc.Name AS SubCategoryName,
+    pc.Name AS CategoryName,
+    p.Weight,
+    p.Size,
+    1 AS IsPurchased
+FROM Production.Product AS p
+INNER JOIN Sales.SalesOrderDetail AS sod ON p.ProductID = sod.ProductID
+LEFT JOIN Production.ProductSubcategory AS psc ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+LEFT JOIN Production.ProductCategory AS pc ON psc.ProductCategoryID = pc.ProductCategoryID;
+
+SELECT * FROM Kubs.DIM_CUSTOMER;
+SELECT * FROM Kubs.DIM_PRODUCT;
+SELECT * FROM Kubs.DIM_SALESPERSON;
+SELECT * FROM Kubs.FACT_SALES;
