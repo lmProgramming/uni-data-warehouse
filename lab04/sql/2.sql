8682c9f8f4ba1a48b0d42dc098d8f5d1876bb2f8
lmProgramming <mikikubs@gmail.com>
2025-04-10 12:49:30 +0200

    feat: 1 and 2 done

---
+++ lab04/sql/2.sql
@@ -0,0 +1,49 @@
+DROP TABLE Kubs.DIM_CUSTOMER;
+DROP TABLE Kubs.DIM_PRODUCT;
+DROP TABLE Kubs.DIM_SALESPERSON;
+DROP TABLE Kubs.FACT_SALES;
+
+CREATE TABLE Kubs.DIM_CUSTOMER (
+    CustomerID INT NOT NULL,
+    FirstName NVARCHAR(50) NULL,
+    LastName NVARCHAR(50) NULL,
+    Title NVARCHAR(8) NULL,
+    City NVARCHAR(30) NULL,
+    TerritoryName NVARCHAR(50) NULL,
+    CountryRegionCode NVARCHAR(3) NULL,
+    [Group] NVARCHAR(50) NULL
+);
+
+CREATE TABLE Kubs.DIM_PRODUCT (
+    ProductID INT NOT NULL,
+    Name NVARCHAR(50) NOT NULL,
+    ListPrice MONEY NULL,
+    Color NVARCHAR(15) NULL,
+    SubCategoryName NVARCHAR(50) NULL,
+    CategoryName NVARCHAR(50) NULL,
+    Weight DECIMAL(8, 2) NULL,
+    Size NVARCHAR(5) NULL,
+    IsPurchased BIT NULL DEFAULT 0
+);
+
+CREATE TABLE Kubs.DIM_SALESPERSON (
+    SalesPersonID INT NOT NULL,
+    FirstName NVARCHAR(50) NULL,
+    LastName NVARCHAR(50) NULL,
+    Title NVARCHAR(8) NULL,
+    Gender NCHAR(1) NULL,
+    CountryRegionCode NVARCHAR(3) NULL,
+    [Group] NVARCHAR(50) NULL
+);
+
+CREATE TABLE Kubs.FACT_SALES (
+    ProductID INT NOT NULL,
+    CustomerID INT NOT NULL,
+    SalesPersonID INT NULL,      
+    OrderDate INT NOT NULL,        
+    ShipDate INT NULL,             
+    OrderQty SMALLINT NOT NULL, 
+    UnitPrice MONEY NOT NULL,
+    UnitPriceDiscount DECIMAL(8, 4) NOT NULL DEFAULT 0,
+    LineTotal DECIMAL(19, 4) NOT NULL
+);
