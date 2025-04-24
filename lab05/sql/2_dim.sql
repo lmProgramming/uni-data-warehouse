0a94f32214284f3fcacb9b22ae141b6cc1411fe2
lmProgramming <mikikubs@gmail.com>
2025-04-24 20:43:26 +0200

    reformat: sql formatter

--- lab05/sql/2_dim.sql
+++ lab05/sql/2_dim.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Kubs.DIM_TIME (
-    PK_TIME INT PRIMARY KEY,      
-    FullDate DATE NOT NULL,       
+    PK_TIME INT PRIMARY KEY,
+    FullDate DATE NOT NULL,
     Rok INT NOT NULL,
-    Kwarta³ INT NOT NULL,
-    Miesi¹c INT NOT NULL,              
-    Miesi¹c_s³ownie NVARCHAR(20) NOT NULL,
-    Dzieñ_tyg INT NOT NULL,         
-    Dzieñ_tyg_s³ownie NVARCHAR(20) NOT NULL,
-    Dzieñ_miesi¹ca INT NOT NULL    
+    Kwartal INT NOT NULL,
+    Miesiac INT NOT NULL,
+    Miesiac_slownie NVARCHAR(20) NOT NULL,
+    Dzien_tyg INT NOT NULL,
+    Dzien_tyg_slownie NVARCHAR(20) NOT NULL,
+    Dzien_miesiaca INT NOT NULL
 );
--- lab05/sql/2_seeding.sql
+++ lab05/sql/2_seeding.sql
@@ -1,37 +1,44 @@
--- Efficiently get all unique dates needed from the source
 WITH SourceDates AS (
-    SELECT DISTINCT OrderDate AS CalendarDate
-    FROM Sales.SalesOrderHeader
-    WHERE OrderDate IS NOT NULL
-    UNION -- UNION handles DISTINCT automatically
-    SELECT DISTINCT ShipDate AS CalendarDate
-    FROM Sales.SalesOrderHeader
-    WHERE ShipDate IS NOT NULL
+    SELECT
+        DISTINCT OrderDate AS CalendarDate
+    FROM
+        Sales.SalesOrderHeader
+    WHERE
+        OrderDate IS NOT NULL
+    UNION
+    SELECT
+        DISTINCT ShipDate AS CalendarDate
+    FROM
+        Sales.SalesOrderHeader
+    WHERE
+        ShipDate IS NOT NULL
 )
-INSERT INTO Kubs.DIM_TIME (
-    PK_TIME,
-    FullDate,
-    Rok,
-    Kwarta³,
-    Miesi¹c,
-    Miesi¹c_s³ownie,
-    Dzieñ_tyg,
-    Dzieñ_tyg_s³ownie,
-    Dzieñ_miesi¹ca
-)
+INSERT INTO
+    Kubs.DIM_TIME (
+        PK_TIME,
+        FullDate,
+        Rok,
+        Kwartal,
+        Miesiac,
+        Miesiac_slownie,
+        Dzien_tyg,
+        Dzien_tyg_slownie,
+        Dzien_miesiaca
+    )
 SELECT
     (DATEPART(year, sd.CalendarDate) * 10000) + (DATEPART(month, sd.CalendarDate) * 100) + DATEPART(day, sd.CalendarDate) AS PK_TIME,
     sd.CalendarDate AS FullDate,
     DATEPART(year, sd.CalendarDate) AS Rok,
-    DATEPART(quarter, sd.CalendarDate) AS Kwarta³,
-    DATEPART(month, sd.CalendarDate) AS Miesi¹c,
-    ISNULL(hm.MonthName_PL, 'Unknown') AS Miesi¹c_s³ownie, -- Join to helper table
-    DATEPART(weekday, sd.CalendarDate) AS Dzieñ_tygodnia,
-    ISNULL(hwd.WeekdayName_PL, 'Unknown') AS Dzieñ_tygodnia_s³ownie, -- Join to helper table
-    DATEPART(day, sd.CalendarDate) AS Dzieñ_miesi¹ca
-FROM SourceDates sd
-LEFT JOIN Kubs.Helper_Months hm ON DATEPART(month, sd.CalendarDate) = hm.MonthNum
-LEFT JOIN Kubs.Helper_Weekdays hwd ON DATEPART(weekday, sd.CalendarDate) = hwd.WeekdayNum; -- Ensure WeekdayNum matches DATEPART output
+    DATEPART(quarter, sd.CalendarDate) AS Kwartal,
+    DATEPART(month, sd.CalendarDate) AS Miesiac,
+    ISNULL(hm.MonthName_PL, 'Unknown') AS Miesiac_slownie,
+    DATEPART(weekday, sd.CalendarDate) AS Dzien_tygodnia,
+    ISNULL(hwd.WeekdayName_PL, 'Unknown') AS Dzien_tygodnia_slownie,
+    DATEPART(day, sd.CalendarDate) AS Dzien_miesiaca
+FROM
+    SourceDates sd
+    LEFT JOIN Kubs.Helper_Months hm ON DATEPART(month, sd.CalendarDate) = hm.MonthNum
+    LEFT JOIN Kubs.Helper_Weekdays hwd ON DATEPART(weekday, sd.CalendarDate) = hwd.WeekdayNum;
 
 -- Add FK constraints from FACT_SALES to DIM_TIME (will be done in Reference step)
 -- ALTER TABLE Kubs.FACT_SALES ADD CONSTRAINT FK_FACT_SALES_DIM_TIME_OrderDate FOREIGN KEY (OrderDate) REFERENCES Kubs.DIM_TIME(PK_TIME);
