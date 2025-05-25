cb1c03ea0e28ef9b87f4b8a7359e1cf3b23d2fa5
lmProgramming <mikikubs@gmail.com>
2025-05-25 11:07:04 +0200

    fix: fixed incorrect counts in fact dimension

--- project/stage02/sql/3_insert_helpers.sql
+++ project/stage02/sql/3_insert_helpers.sql
@@ -17,13 +17,13 @@ VALUES
 INSERT INTO
     Helper_Weekdays (WeekdayNum, WeekdayName)
 VALUES
-    (1, 'Sunday'),
-    (2, 'Monday'),
-    (3, 'Tuesday'),
-    (4, 'Wednesday'),
-    (5, 'Thursday'),
-    (6, 'Friday'),
-    (7, 'Saturday');
+    (7, 'Sunday'),
+    (1, 'Monday'),
+    (2, 'Tuesday'),
+    (3, 'Wednesday'),
+    (4, 'Thursday'),
+    (5, 'Friday'),
+    (6, 'Saturday');
 
 INSERT INTO
     Helper_CountryContinents (CountryName, Continent)
